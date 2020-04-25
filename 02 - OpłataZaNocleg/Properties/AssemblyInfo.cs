dd620903852d36daff70c1a19432d1c67c371204
Mariusz Gburczyk <LarryUgwu@outlook.com>
2020-04-25 19:33:50 +0200

    korekta

--- 02 - OpłataZaNocleg/Properties/AssemblyInfo.cs
+++ 02 - OpłataZaNocleg/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("02 - OpłataZaNocleg")]
+[assembly: AssemblyTitle("OpłataZaNocleg")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("02 - OpłataZaNocleg")]
+[assembly: AssemblyProduct("OpłataZaNocleg")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
